d7c3280e77068a45f1bf8e6f432dd4e533d855c5
jordyellis <jdaellis@icloud.com>
2024-02-07 18:12:25 -0500

    Made_ball_with_Colliders

--- Pong_Jordan_Ellis/Assets/Scripts/Ball controller.cs
+++ Pong_Jordan_Ellis/Assets/Scripts/Ball controller.cs
@@ -4,10 +4,41 @@ using UnityEngine;
 
 public class Ballcontroller : MonoBehaviour
 {
+
+    public Rigidbody2D rbBall;
+
+    public float force = 200;
+
+    private float xDir;
+    private float yDir;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello world");
+        Vector3 direction = new Vector3(0, 0, 0);
+        
+        yDir = Random.Range(0, 2);
+        // Debug.Log("yDir = " + yDir);
+        if (yDir == 0)
+        {
+            direction.y = -1;
+        } else if (yDir == 1)
+        {
+            direction.y = 1;
+        }
+        
+        xDir = Random.Range(0, 2);
+        // Debug.Log("xDir = " + xDir);
+        if (xDir == 0)
+        {
+            direction.x = -1;
+        } else if (xDir == 1)
+        {
+            direction.x = 1;
+        }
+        
+        // add force to start movement
+        rbBall.AddForce(direction * force);
     }
 
     // Update is called once per frame
