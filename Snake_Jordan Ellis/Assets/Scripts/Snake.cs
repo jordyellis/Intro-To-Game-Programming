94d6d155e633c9babaa72b504e10c4a8c1aa4ac7
jordyellis <jdaellis@icloud.com>
2024-03-04 18:27:05 -0500

    Made Snake move

--- Snake_Jordan Ellis/Assets/Scripts/Snake.cs
+++ Snake_Jordan Ellis/Assets/Scripts/Snake.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Snake : MonoBehaviour
 {
@@ -11,12 +12,14 @@ public class Snake : MonoBehaviour
                                  //Remember the difference b/w Vector2(x,y) & Vector3(x,y,z)
                                  //there is shorthand for Vector2/3 directions.
                                  //In this one, Vector3.right = Vector3(1,0,0), moving it to the RIGHT
+    List<Transform> tail = new List<Transform>();
+    bool ate = false;
+    public GameObject tailPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         //OPTION: Randomize the direction of the snake like our Pong Ball when it starts
-
         //Call MoveSnake() every 300ms(0.3 seconds) to move the snake
         InvokeRepeating("MoveSnake", 0.3f, 0.3f);
         //InvokeRepeating("methodName", time, repeatRate)
@@ -36,12 +39,34 @@ public class Snake : MonoBehaviour
 
     void MoveSnake()
     {
+        Vector3 gap = transform.position;
+
         //In Snake, the snake is ALWAYS moving in some direction, never standing still.
         //MOVE SNAKE HEAD IN A DIRECTION
         transform.Translate(dir);
         //Translate moves the transform property in the direction and distance of the translation,
         //In this case, we are moving in the whatever direction we've set the dir variable to be the distance of that variable
 
+        if(ate)
+        {
+            Debug.Log("Ate =" + ate);
+
+            GameObject tailSec = (GameObject)Instantiate(tailPrefab, gap, Quaternion.identity);
+            tail.Insert(0, tailSec.transform);
+
+            ate = false;
+        }
+        //check if snake has tail
+        else if (tail.Count > 0)
+        {
+            //move last tail to where head was previously was
+            tail.Last().position = gap;
+
+            //keep tail list in order
+            //add last element tail to the front of the list and remove from the back
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count - 1);
+        }
     }
 
     private void ChangeDirection()
@@ -74,6 +99,7 @@ public class Snake : MonoBehaviour
         //When Snake collides with Food...
         if (collision.gameObject.tag == "Food")
         {
+            ate = true;
             //Debug.Log("food eaten");
             Destroy(collision.gameObject); //Remove the Food
 
